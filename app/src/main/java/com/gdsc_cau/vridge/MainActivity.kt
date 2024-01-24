1431d193200255076e798b19d82210f9f9befd34
yymin1022 <yymin1022@gmail.com>
2024-01-24 19:10:22 +0900

    Update MainActivity : Cleaned UP

--- app/src/main/java/com/gdsc_cau/vridge/MainActivity.kt
+++ app/src/main/java/com/gdsc_cau/vridge/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.gdsc_cau.vridge.ui.theme.VridgeTheme
 
 class MainActivity : ComponentActivity() {
