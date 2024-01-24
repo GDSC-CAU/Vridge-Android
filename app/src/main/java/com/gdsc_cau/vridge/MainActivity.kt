05e51a56ea4a5b0580f87c8a6ec5ea1eebc2fdf3
yymin1022 <yymin1022@gmail.com>
2024-01-24 18:47:50 +0900

    Update MainActivity : Removed Composable Preview

--- app/src/main/java/com/gdsc_cau/vridge/MainActivity.kt
+++ app/src/main/java/com/gdsc_cau/vridge/MainActivity.kt
@@ -36,11 +36,3 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VridgeTheme {
-        Greeting("Android")
-    }
-}
