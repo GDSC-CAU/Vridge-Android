46d4ed5d7aecb2641cb6bb1f6460ec6c00cf225c
hoon <ldh019@naver.com>
2024-01-24 19:14:13 +0900

    Update Theme.kt : Change Theme Colors

--- app/src/main/java/com/gdsc_cau/vridge/ui/theme/Theme.kt
+++ app/src/main/java/com/gdsc_cau/vridge/ui/theme/Theme.kt
@@ -16,32 +16,30 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = DarkPrimary,
+    onPrimary = PrimaryLight,
+    surface = OnPrimaryLight,
+    onSurface = Primary,
+    inversePrimary = PrimaryDark,
+    primaryContainer = PrimaryUpperLight,
+    background = Grey1
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Primary,
+    onPrimary = OnPrimary,
+    surface = PrimaryLight,
+    onSurface = OnPrimaryLight,
+    inversePrimary = PrimaryDark,
+    primaryContainer = PrimaryUpperLight,
+    background = White,
 )
 
 @Composable
 fun VridgeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
