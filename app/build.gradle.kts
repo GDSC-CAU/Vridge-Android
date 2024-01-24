3bc46a8e77201b50fd8ccd5b0ec97a5885a82709
hoon <ldh019@naver.com>
2024-01-24 19:48:03 +0900

    Update app/build.gradle : Add Retrofit Implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,8 @@ dependencies {
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
     kapt("com.google.dagger:hilt-android-compiler:2.48")
 
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
