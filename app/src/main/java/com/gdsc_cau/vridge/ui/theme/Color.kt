334e659308e0d2de76ad74f18be36d9dd1f29ba6
hoon <ldh019@naver.com>
2024-01-24 19:14:01 +0900

    Update Color.kt : Added Main Colors

--- app/src/main/java/com/gdsc_cau/vridge/ui/theme/Color.kt
+++ app/src/main/java/com/gdsc_cau/vridge/ui/theme/Color.kt
@@ -2,10 +2,18 @@ package com.gdsc_cau.vridge.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Primary = Color(0xFF5EDCC5)
+val PrimaryDark = Color(0xFF4BAD9B)
+val PrimaryLight = Color(0xFFDBFEF6)
+val PrimaryUpperLight = Color(0xFFA3F7DE)
+val OnPrimaryLight = Color(0xFF183A34)
+val OnPrimary = Color(0xFF142F2A)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkPrimary = Color (0xFF398779)
+
+val Black = Color(0xFF010101)
+val Grey1 = Color(0xFF4F4F4F)
+val Grey2 = Color(0xFF828282)
+val Grey3 = Color(0xFFE0E0E0)
+val Grey4 = Color(0xFFF5F5F7)
+val White = Color(0xFFFFFFFF)
