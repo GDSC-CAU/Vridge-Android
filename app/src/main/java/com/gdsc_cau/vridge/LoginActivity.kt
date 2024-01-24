9e4ee6c70db34c9817aa5379d355b6524882676d
yymin1022 <yymin1022@gmail.com>
2024-01-24 18:48:27 +0900

    Update LoginActivity : Removed Composable Preview

--- app/src/main/java/com/gdsc_cau/vridge/LoginActivity.kt
+++ app/src/main/java/com/gdsc_cau/vridge/LoginActivity.kt
@@ -36,11 +36,3 @@ fun Greeting2(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview2() {
-    VridgeTheme {
-        Greeting2("Android")
-    }
-}
