fda0a2d3a9d1e204c40f309618bb37fed4e1a638
yymin1022 <yymin1022@gmail.com>
2024-01-26 23:22:35 +0900

    Update LoginView : Added Basic UI Elements

--- app/src/main/java/com/gdsc_cau/vridge/Login/LoginView.kt
+++ app/src/main/java/com/gdsc_cau/vridge/Login/LoginView.kt
@@ -1,26 +1,50 @@
 package com.gdsc_cau.vridge.Login
 
 import android.content.Intent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.gdsc_cau.vridge.MainActivity
 
 @Composable
 fun LoginView() {
-    Column {
-        LoginButton()
+    val modifier = Modifier
+
+    Column(
+        modifier = modifier
+            .padding(all = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        LoginLogo(modifier = modifier)
+        LoginButton(modifier = modifier)
     }
+}
+
+@Composable
+fun LoginLogo(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(width = 200.dp, height = 200.dp),
+        painter = painterResource(id = android.R.mipmap.sym_def_app_icon),
+        contentDescription = "Login Logo"
+    )
 }
 
 @Composable
 fun LoginButton(modifier: Modifier = Modifier) {
     val context = LocalContext.current
-    
+
     ElevatedButton(
         modifier = modifier
             .fillMaxWidth(),
