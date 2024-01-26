f58fdc5efeb7578a50c550c38cf83e223cdc75d8
yymin1022 <yymin1022@gmail.com>
2024-01-27 03:00:40 +0900

    Update LoginView : Cleaned UP

--- app/src/main/java/com/gdsc_cau/vridge/login/LoginView.kt
+++ app/src/main/java/com/gdsc_cau/vridge/login/LoginView.kt
@@ -8,13 +8,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
