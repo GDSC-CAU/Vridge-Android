34c871ab4191f2deddf7c75df2778a10fff598db
yymin1022 <yymin1022@gmail.com>
2024-01-27 04:10:56 +0900

    Update FirebaseAuthUtil : Added Functions for Firebase Google Sign-In

--- app/src/main/java/com/gdsc_cau/vridge/utils/FirebaseAuthUtil.kt
+++ app/src/main/java/com/gdsc_cau/vridge/utils/FirebaseAuthUtil.kt
@@ -1,4 +1,32 @@
 package com.gdsc_cau.vridge.utils
 
-class FirebaseAuthUtil {
+import android.content.Intent
+import androidx.activity.result.ActivityResultLauncher
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
+
+object FirebaseAuthUtil {
+    private lateinit var auth: FirebaseAuth
+
+    private fun initFirebaseAuth() {
+        auth = Firebase.auth
+    }
+
+    fun getCurrentUser(): FirebaseUser? {
+        return auth.currentUser
+    }
+
+    fun tryGoogleLogin(signInLauncher: ActivityResultLauncher<Intent>) {
+        initFirebaseAuth()
+
+        val signInProvider = arrayListOf(AuthUI.IdpConfig.GoogleBuilder().build())
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(signInProvider)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
 }
