2dcf5b619c7049efd88f06d2377a78a16c0f6082
yymin1022 <yymin1022@gmail.com>
2024-01-27 02:54:33 +0900

    Update LoginViewModel : Added Basic Login Function

--- app/src/main/java/com/gdsc_cau/vridge/login/LoginViewModel.kt
+++ app/src/main/java/com/gdsc_cau/vridge/login/LoginViewModel.kt
@@ -1,5 +1,13 @@
 package com.gdsc_cau.vridge.login
 
-class LoginViewModel {
+import android.content.Context
+import android.content.Intent
+import com.gdsc_cau.vridge.MainActivity
 
+class LoginViewModel constructor(
+    private val context: Context
+) {
+    fun tryGoogleLogin() {
+        context.startActivity(Intent(context, MainActivity::class.java))
+    }
 }
