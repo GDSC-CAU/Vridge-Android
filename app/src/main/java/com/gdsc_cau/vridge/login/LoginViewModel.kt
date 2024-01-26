288ccb2791eb750d7bc66f6591a3fdaa57ffdc34
yymin1022 <yymin1022@gmail.com>
2024-01-27 02:59:21 +0900

    Update LoginViewModel : Fixed Context Leak Issue

--- app/src/main/java/com/gdsc_cau/vridge/login/LoginViewModel.kt
+++ app/src/main/java/com/gdsc_cau/vridge/login/LoginViewModel.kt
@@ -1,13 +1,15 @@
 package com.gdsc_cau.vridge.login
 
-import android.content.Context
+import android.app.Application
 import android.content.Intent
+import androidx.lifecycle.ViewModel
 import com.gdsc_cau.vridge.MainActivity
 
-class LoginViewModel constructor(
-    private val context: Context
-) {
+class LoginViewModel(
+    private val application: Application
+): ViewModel() {
     fun tryGoogleLogin() {
+        val context = application.applicationContext
         context.startActivity(Intent(context, MainActivity::class.java))
     }
 }
