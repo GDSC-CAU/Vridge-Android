eaad6df15780cfbef5a204412c98bc9e3710968e
yymin1022 <yymin1022@gmail.com>
2024-01-27 03:11:27 +0900

    Update Project : Added Activity Dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.activity:activity-ktx:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
 
     implementation("com.google.dagger:hilt-android:2.48")
