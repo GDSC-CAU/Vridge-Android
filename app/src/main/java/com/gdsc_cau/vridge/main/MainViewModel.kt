5e74bd3514bfccf15224ee2ab01a8b29a269b926
hoon <ldh019@naver.com>
2024-01-27 22:05:42 +0900

    Update MainViewModel : Fix for ktlint

--- app/src/main/java/com/gdsc_cau/vridge/main/MainViewModel.kt
+++ app/src/main/java/com/gdsc_cau/vridge/main/MainViewModel.kt
@@ -2,10 +2,13 @@ package com.gdsc_cau.vridge.main
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel : ViewModel() {
-    fun getGreeting(name: String): String {
-        return "Hello $name!"
-    }
+class MainViewModel
+    @Inject
+    constructor() : ViewModel() {
+        fun getGreeting(name: String): String {
+            return "Hello $name!"
+        }
 }
