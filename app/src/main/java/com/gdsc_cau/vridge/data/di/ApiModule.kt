44bd09ca1156c658b2a7969ab7f5b38ad1deead0
hoon <ldh019@naver.com>
2024-02-14 21:06:25 +0900

    Update ApiModule : Add OkHttpClient Inject Function

--- app/src/main/java/com/gdsc_cau/vridge/data/di/ApiModule.kt
+++ app/src/main/java/com/gdsc_cau/vridge/data/di/ApiModule.kt
@@ -1,4 +1,18 @@
 package com.gdsc_cau.vridge.data.di
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object ApiModule {
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().build()
+    }
 }
