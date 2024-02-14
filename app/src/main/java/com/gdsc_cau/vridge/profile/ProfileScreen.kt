ad4812a9ce1b2d0eaf1b43ef3a0707ad277804c1
yymin1022 <yymin1022@gmail.com>
2024-02-14 16:37:51 +0900

    Update ProfileScreen : Added DummyData

--- app/src/main/java/com/gdsc_cau/vridge/profile/ProfileScreen.kt
+++ app/src/main/java/com/gdsc_cau/vridge/profile/ProfileScreen.kt
@@ -1,9 +1,25 @@
 package com.gdsc_cau.vridge.profile
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import com.gdsc_cau.vridge.main.Greeting
+import com.gdsc_cau.vridge.models.Gender
+import com.gdsc_cau.vridge.models.User
+
+val profileDummy =
+    User(
+        cntVoice = 0,
+        email = "hello_vridge@gmail.com",
+        gender = Gender.MALE,
+        name = "Vridge Test User",
+        uid = "VRIDGE_TEST_USER_UID"
+    )
 
 @Composable
 fun ProfileScreen() {
-    Greeting(name = "Profile")
+    ProfileList(profileData = profileDummy)
+}
+
+@Composable
+fun ProfileList(profileData: User) {
+    Text(text = profileData.name)
 }
