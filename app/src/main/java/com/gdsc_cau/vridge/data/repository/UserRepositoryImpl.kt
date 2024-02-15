19f13418d6de453d99586a33369c3a9f9cc37813
hoon <ldh019@naver.com>
2024-02-15 17:21:49 +0900

    Update UserRepositoryImpl : Add functions

--- app/src/main/java/com/gdsc_cau/vridge/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/gdsc_cau/vridge/data/repository/UserRepositoryImpl.kt
@@ -1,4 +1,30 @@
 package com.gdsc_cau.vridge.data.repository
 
-class UserRepositoryImpl {
-}
+import com.gdsc_cau.vridge.data.api.VridgeApi
+import com.gdsc_cau.vridge.data.database.InfoDatabase
+import com.gdsc_cau.vridge.data.models.User
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import javax.inject.Inject
+
+class UserRepositoryImpl
+    @Inject
+    constructor(
+        private val api: VridgeApi,
+        private val database: InfoDatabase
+    ) : UserRepository {
+        override suspend fun login(token: String): Boolean {
+            val data =
+                JsonObject(
+                    mapOf(
+                        "token" to JsonPrimitive(token)
+                    )
+                )
+            val result = api.login(data)
+            return result.isSuccess
+        }
+
+        override suspend fun getUser(uid: String): User {
+            return database.getUser(uid)
+        }
+    }
