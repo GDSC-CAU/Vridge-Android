6a7d5e4da758c600171faae0c86f92869566ca9d
hoon <ldh019@naver.com>
2024-02-15 17:18:39 +0900

    Update UserRepository : Add functions

--- app/src/main/java/com/gdsc_cau/vridge/data/repository/UserRepository.kt
+++ app/src/main/java/com/gdsc_cau/vridge/data/repository/UserRepository.kt
@@ -1,4 +1,9 @@
 package com.gdsc_cau.vridge.data.repository
 
+import com.gdsc_cau.vridge.data.models.User
+
 interface UserRepository {
+    suspend fun login(token: String): Boolean
+
+    suspend fun getUser(uid: String): User
 }
