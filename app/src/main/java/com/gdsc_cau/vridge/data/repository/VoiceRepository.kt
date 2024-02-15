5cadc52dee18a3dc3858871ab24d0ad83503b351
hoon <ldh019@naver.com>
2024-02-15 17:19:58 +0900

    Update VoiceRepository : Add functions

--- app/src/main/java/com/gdsc_cau/vridge/data/repository/VoiceRepository.kt
+++ app/src/main/java/com/gdsc_cau/vridge/data/repository/VoiceRepository.kt
@@ -1,4 +1,13 @@
 package com.gdsc_cau.vridge.data.repository
 
 interface VoiceRepository {
+    fun getTrainingText(index: Int): String
+
+    fun setTrainingVoice(index: Int, filePath: String): Boolean
+
+    fun makeVoice(uid: String, path: String)
+
+    fun synthesize(uid: String, voiceId: List<String>)
+
+    fun getVoiceList(uid: String): List<String>
 }
