77fcb2b0ef9a065a398f2825c8b4a8b9511e8566
hoon <ldh019@naver.com>
2024-02-16 12:23:41 +0900

    Update VoiceRepositoryImpl : Add function signatures

--- app/src/main/java/com/gdsc_cau/vridge/data/repository/VoiceRepositoryImpl.kt
+++ app/src/main/java/com/gdsc_cau/vridge/data/repository/VoiceRepositoryImpl.kt
@@ -1,4 +1,35 @@
 package com.gdsc_cau.vridge.data.repository
 
-class VoiceRepositoryImpl {
+import android.app.Application
+
+class VoiceRepositoryImpl : VoiceRepository {
+    private val assetManager = Application().assets
+    private val scripts =
+        assetManager.open("script.txt")
+            .let {
+                val size: Int = it.available()
+                val buffer = ByteArray(size)
+                it.read(buffer)
+                String(buffer).split("\n")
+            }
+
+    override fun getTrainingText(index: Int): String {
+        return scripts[index]
+    }
+
+    override fun setTrainingVoice(index: Int, filePath: String): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun makeVoice(uid: String, path: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun synthesize(uid: String, voiceId: List<String>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getVoiceList(uid: String): List<String> {
+        TODO("Not yet implemented")
+    }
 }
