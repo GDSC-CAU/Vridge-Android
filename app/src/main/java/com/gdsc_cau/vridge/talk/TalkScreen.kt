c8e1f35d4df09df07646b1e42585d5b2f72a30ec
yymin1022 <yymin1022@gmail.com>
2024-02-17 20:25:16 +0900

    Update TalkScreen : Added TalkInput Compose Widget

--- app/src/main/java/com/gdsc_cau/vridge/talk/TalkScreen.kt
+++ app/src/main/java/com/gdsc_cau/vridge/talk/TalkScreen.kt
@@ -1,10 +1,16 @@
 package com.gdsc_cau.vridge.talk
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -36,5 +42,23 @@ fun TalkCard() {
                 .height(40.dp)
     ) {
         Text(text = "Talk Card")
+    }
+}
+
+@Composable
+fun TalkInput(message: String, onClicked: () -> Unit) {
+    Row {
+        TextField(
+            value = "",
+            onValueChange = {}
+        )
+        IconButton(
+            onClick = onClicked
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Send,
+                contentDescription = ""
+            )
+        }
     }
 }
