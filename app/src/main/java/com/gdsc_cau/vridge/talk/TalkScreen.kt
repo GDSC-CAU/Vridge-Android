70b1196db7a223cd456f44b89f62e31386d67da0
yymin1022 <yymin1022@gmail.com>
2024-02-18 02:59:57 +0900

    Update TalkScreen : Enabled Preview for Debug

--- app/src/main/java/com/gdsc_cau/vridge/talk/TalkScreen.kt
+++ app/src/main/java/com/gdsc_cau/vridge/talk/TalkScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.gdsc_cau.vridge.ui.theme.Black
 import com.gdsc_cau.vridge.ui.theme.White
@@ -38,9 +39,10 @@ private enum class VoiceState {
     VOICE_READY
 }
 
+@Preview
 @Composable
 fun TalkScreen(
-    sessionId: String
+//    sessionId: String
 ) {
     TalkHistory()
     TalkInput {}
