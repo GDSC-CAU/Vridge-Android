8fc04c90c2613227386dc83440e46412debcbad2
Hoon <ldh019@naver.com>
2024-02-17 19:55:37 +0900

    Update VoiceRepository : Add suspend keyword to functions

--- app/src/main/java/com/gdsc_cau/vridge/data/repository/VoiceRepository.kt
+++ app/src/main/java/com/gdsc_cau/vridge/data/repository/VoiceRepository.kt
@@ -3,11 +3,11 @@ package com.gdsc_cau.vridge.data.repository
 interface VoiceRepository {
     fun getTrainingText(index: Int): String
 
-    fun setTrainingVoice(index: Int, filePath: String): Boolean
+    suspend fun setTrainingVoice(index: Int, filePath: String): Boolean
 
-    fun makeVoice(uid: String, path: String)
+    suspend fun makeVoice(uid: String, path: String)
 
-    fun synthesize(uid: String, voiceId: List<String>)
+    suspend fun synthesize(uid: String, voiceId: List<String>)
 
-    fun getVoiceList(uid: String): List<String>
+    suspend fun getVoiceList(uid: String): List<String>
 }
