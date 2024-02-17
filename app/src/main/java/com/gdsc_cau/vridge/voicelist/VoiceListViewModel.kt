b94740e5a07a96ad78e36dca46bb02b430c4228d
Hoon <ldh019@naver.com>
2024-02-17 09:18:06 +0900

    Added VoiceListViewModel

---
+++ app/src/main/java/com/gdsc_cau/vridge/voicelist/VoiceListViewModel.kt
@@ -0,0 +1,11 @@
+package com.gdsc_cau.vridge.voicelist
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class VoiceListViewModel
+    @Inject
+    constructor() {
+
+}
