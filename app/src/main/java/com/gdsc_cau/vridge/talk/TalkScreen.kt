625cb21eef4ec6e9a85b3aafc2731ed52b43b86c
yymin1022 <yymin1022@gmail.com>
2024-02-17 20:24:28 +0900

    Update TalkScreen : Added TalkCard Compose Widget

--- app/src/main/java/com/gdsc_cau/vridge/talk/TalkScreen.kt
+++ app/src/main/java/com/gdsc_cau/vridge/talk/TalkScreen.kt
@@ -1,6 +1,13 @@
 package com.gdsc_cau.vridge.talk
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.gdsc_cau.vridge.main.Greeting
 
 private enum class VoiceState {
@@ -14,4 +21,20 @@ fun TalkScreen(
     sessionId: String
 ) {
     Greeting(name = "Talk $sessionId")
+}
+
+@Composable
+fun TalkCard() {
+    ElevatedCard(
+        elevation =
+            CardDefaults.cardElevation(
+                defaultElevation = 6.dp
+            ),
+        modifier =
+            Modifier
+                .fillMaxWidth()
+                .height(40.dp)
+    ) {
+        Text(text = "Talk Card")
+    }
 }
