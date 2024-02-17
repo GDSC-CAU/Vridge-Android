db3b977b459a73730d83ec5dae287add85265ebd
Hoon <ldh019@naver.com>
2024-02-17 19:55:12 +0900

    Update UserRepository : Add unregister function

--- app/src/main/java/com/gdsc_cau/vridge/data/repository/UserRepository.kt
+++ app/src/main/java/com/gdsc_cau/vridge/data/repository/UserRepository.kt
@@ -5,5 +5,7 @@ import com.gdsc_cau.vridge.data.models.User
 interface UserRepository {
     suspend fun login(token: String): Boolean
 
+    suspend fun unregister(uid: String): Boolean
+
     suspend fun getUser(uid: String): User
 }
