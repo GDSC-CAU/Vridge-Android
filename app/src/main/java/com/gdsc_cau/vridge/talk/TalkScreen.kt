1bc81cd7f2496e43c8011b9d4155652f58241a49
yymin1022 <yymin1022@gmail.com>
2024-02-17 18:53:01 +0900

    Update TalkScreen : Added Enum Class for Voice State

--- app/src/main/java/com/gdsc_cau/vridge/talk/TalkScreen.kt
+++ app/src/main/java/com/gdsc_cau/vridge/talk/TalkScreen.kt
@@ -3,6 +3,12 @@ package com.gdsc_cau.vridge.talk
 import androidx.compose.runtime.Composable
 import com.gdsc_cau.vridge.main.Greeting
 
+private enum class VoiceState {
+    VOICE_LOADING,
+    VOICE_PLAYING,
+    VOICE_READY
+}
+
 @Composable
 fun TalkScreen(
     sessionId: String
