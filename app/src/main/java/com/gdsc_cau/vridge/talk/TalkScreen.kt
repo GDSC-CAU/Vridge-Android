93bd4a3cfb8019c36a9309cd8fd900e4557c8a86
yymin1022 <yymin1022@gmail.com>
2024-02-17 23:25:48 +0900

    Update TalkScreen : Applied BasicTextField for Decoration

--- app/src/main/java/com/gdsc_cau/vridge/talk/TalkScreen.kt
+++ app/src/main/java/com/gdsc_cau/vridge/talk/TalkScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Send
@@ -17,8 +18,11 @@ import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -91,19 +95,28 @@ private fun TalkCard(talkData: String, talkState: VoiceState) {
 
 @Composable
 fun TalkInput(onClicked: () -> Unit) {
+    var data by remember {
+        mutableStateOf("")
+    }
+
     Row(
         modifier =
             Modifier
                 .fillMaxSize(),
         verticalAlignment = Alignment.Bottom
     ) {
-        TextField(
+        BasicTextField(
             modifier =
                 Modifier
                     .height(50.dp)
                     .weight(1f),
-            value = "",
-            onValueChange = {}
+            value = data,
+            onValueChange = { input ->
+                data = input
+            },
+            decorationBox = { innerTextField ->
+                TalkInputDecor(innerTextField)
+            }
         )
         IconButton(
             modifier =
@@ -117,5 +130,14 @@ fun TalkInput(onClicked: () -> Unit) {
                 contentDescription = "Send Button"
             )
         }
+    }
+}
+
+@Composable
+fun TalkInputDecor(innerTextField: @Composable () -> Unit) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        innerTextField()
     }
 }
