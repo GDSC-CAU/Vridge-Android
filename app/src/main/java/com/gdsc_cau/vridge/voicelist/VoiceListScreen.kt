a915f468621043ae94133a63fc3db69fe41f8619
Hoon <ldh019@naver.com>
2024-02-17 09:18:20 +0900

    Update VoiceListScreen : Add sub components

--- app/src/main/java/com/gdsc_cau/vridge/voicelist/VoiceListScreen.kt
+++ app/src/main/java/com/gdsc_cau/vridge/voicelist/VoiceListScreen.kt
@@ -1,16 +1,48 @@
 package com.gdsc_cau.vridge.voicelist
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.gdsc_cau.vridge.main.Greeting
+import com.gdsc_cau.vridge.main.MainNavigator
+import com.gdsc_cau.vridge.models.Voice
 
 @Composable
 fun VoiceListScreen(
     padding: PaddingValues,
-    onVoiceClick: (String) -> Unit
+    onVoiceClick: (Voice) -> Unit,
+    viewModel: VoiceListViewModel = hiltViewModel()
 ) {
-    Button(onClick = { onVoiceClick("1") }) {
-        Greeting(name = "VoiceListScreen")
+    val voices = viewModel.voices
+
+    if (voices.isEmpty()) {
+        EmptyVoiceList()
+    } else {
+        GridVoiceList(voices, onVoiceClick)
+    }
+}
+
+@Composable
+fun EmptyVoiceList() {
+    Greeting(name = "No voices found")
+}
+
+@Composable
+fun GridVoiceList(voices: List<Voice>, onVoiceClick: (Voice) -> Unit) {
+    LazyVerticalGrid(columns = GridCells.Fixed(3)) {
+
+    }
+}
+
+@Composable
+fun VoiceListItem(
+    voice: Voice,
+    onVoiceClick: (Voice) -> Unit
+) {
+    Button(onClick = { onVoiceClick(voice) }) {
+        Greeting(name = voice.name)
     }
 }
