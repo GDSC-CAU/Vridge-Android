b8558be8a88b75ae408b37c682bf239725ba747b
yymin1022 <yymin1022@gmail.com>
2024-02-17 22:03:47 +0900

    Update TalkScreen : Arranged Basic Elements

--- app/src/main/java/com/gdsc_cau/vridge/talk/TalkScreen.kt
+++ app/src/main/java/com/gdsc_cau/vridge/talk/TalkScreen.kt
@@ -1,9 +1,15 @@
 package com.gdsc_cau.vridge.talk
 
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.CardDefaults
@@ -13,6 +19,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -27,9 +34,30 @@ private enum class VoiceState {
 fun TalkScreen(
     sessionId: String
 ) {
-    Column {
+    TalkHistory()
+    TalkInput {}
+}
+
+@Composable
+fun TalkHistory() {
+    Column(
+        modifier =
+            Modifier
+                .fillMaxSize()
+                .padding(bottom = 60.dp)
+                .verticalScroll(ScrollState(Int.MAX_VALUE)),
+        verticalArrangement = Arrangement.Bottom
+    ) {
         TalkCard()
-        TalkInput {}
+        TalkCard()
+        TalkCard()
+        TalkCard()
+        TalkCard()
+        TalkCard()
+        TalkCard()
+        TalkCard()
+        TalkCard()
+        TalkCard()
     }
 }
 
@@ -54,24 +82,37 @@ fun TalkCard() {
             modifier =
                 Modifier
                     .padding(all = 15.dp),
-            text = "This is Talk Card Content"
+            text = "This is Talk Card Content\nThis is Talk Card Content"
         )
     }
 }
 
 @Composable
 fun TalkInput(onClicked: () -> Unit) {
-    Row {
+    Row(
+        modifier =
+            Modifier
+                .fillMaxSize(),
+        verticalAlignment = Alignment.Bottom
+    ) {
         TextField(
+            modifier =
+                Modifier
+                    .height(50.dp)
+                    .weight(1f),
             value = "",
             onValueChange = {}
         )
         IconButton(
+            modifier =
+                Modifier
+                    .height(45.dp)
+                    .width(45.dp),
             onClick = onClicked
         ) {
             Icon(
                 imageVector = Icons.Filled.Send,
-                contentDescription = ""
+                contentDescription = "Send Button"
             )
         }
     }
