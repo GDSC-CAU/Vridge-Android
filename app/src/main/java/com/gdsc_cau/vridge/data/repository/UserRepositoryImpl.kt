91c2d1914e0a2708adf84ea1bc2f14ee73a5db51
hoon <ldh019@naver.com>
2024-02-19 08:59:00 +0900

    Update UserRepositoryImpl : Implement for new functions

--- app/src/main/java/com/gdsc_cau/vridge/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/gdsc_cau/vridge/data/repository/UserRepositoryImpl.kt
@@ -3,6 +3,8 @@ package com.gdsc_cau.vridge.data.repository
 import com.gdsc_cau.vridge.data.api.VridgeApi
 import com.gdsc_cau.vridge.data.database.InfoDatabase
 import com.gdsc_cau.vridge.data.models.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.JsonPrimitive
 import javax.inject.Inject
@@ -11,7 +13,8 @@ class UserRepositoryImpl
     @Inject
     constructor(
         private val api: VridgeApi,
-        private val database: InfoDatabase
+        private val database: InfoDatabase,
+        private val auth: FirebaseAuth
     ) : UserRepository {
         override suspend fun login(token: String): Boolean {
             val data =
@@ -36,7 +39,15 @@ class UserRepositoryImpl
             return result.isSuccess
         }
 
-        override suspend fun getUser(uid: String): User {
+        override fun getCurrentUser(): FirebaseUser? {
+            return auth.currentUser
+        }
+
+        override fun signOut() {
+            auth.signOut()
+        }
+
+        override suspend fun getUserInfo(uid: String): User {
             return database.getUser(uid)
         }
     }
