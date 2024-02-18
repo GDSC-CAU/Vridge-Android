1140910ce449b84282f372e2f2b6e05b55979c7b
yymin1022 <yymin1022@gmail.com>
2024-02-18 16:36:57 +0900

    Update TalkScreen : Adjust TalkInput Layout

--- app/src/main/java/com/gdsc_cau/vridge/talk/TalkScreen.kt
+++ app/src/main/java/com/gdsc_cau/vridge/talk/TalkScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CardDefaults
@@ -21,7 +22,6 @@ import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
-import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -30,11 +30,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.gdsc_cau.vridge.R
 import com.gdsc_cau.vridge.ui.theme.Black
 import com.gdsc_cau.vridge.ui.theme.Grey3
+import com.gdsc_cau.vridge.ui.theme.Grey4
 import com.gdsc_cau.vridge.ui.theme.PrimaryUpperLight
 import com.gdsc_cau.vridge.ui.theme.White
 
@@ -186,19 +188,19 @@ fun TalkInput(onSendClicked: () -> Unit) {
             contentAlignment = Alignment.Center,
             modifier =
                 Modifier
+                    .background(Grey4)
                     .height(60.dp)
                     .width(60.dp)
         ) {
             IconButton(
                 colors =
                     IconButtonDefaults.iconButtonColors(
-                        containerColor = Grey3,
                         contentColor = Black
                     ),
                 modifier =
                     Modifier
-                        .height(50.dp)
-                        .width(50.dp),
+                        .height(40.dp)
+                        .width(40.dp),
                 onClick = onSendClicked
             ) {
                 Icon(
@@ -213,29 +215,24 @@ fun TalkInput(onSendClicked: () -> Unit) {
 @Composable
 fun TalkInputDecor(innerTextField: @Composable () -> Unit) {
     Row(
+        modifier =
+            Modifier
+                .background(Grey4)
+                .padding(start = 10.dp)
+                .padding(vertical = 10.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        OutlinedCard(
-            colors =
-                CardDefaults.cardColors(
-                    containerColor = White,
-                    contentColor = Black
-                ),
+        Box(
+            contentAlignment = Alignment.CenterStart,
             modifier =
                 Modifier
+                    .clip(RoundedCornerShape(8.dp))
                     .background(White)
                     .fillMaxSize()
-                    .padding(all = 5.dp)
+                    .padding(start = 10.dp)
+                    .padding(vertical = 5.dp)
         ) {
-            Box(
-                contentAlignment = Alignment.CenterStart,
-                modifier =
-                    Modifier
-                        .fillMaxSize()
-                        .padding(horizontal = 10.dp, vertical = 5.dp)
-            ) {
-                innerTextField()
-            }
+            innerTextField()
         }
     }
 }
