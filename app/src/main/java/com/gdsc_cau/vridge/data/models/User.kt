ab90aaf25a1d023b7a269e340dcca29bcd9de666
hoon <ldh019@naver.com>
2024-02-19 18:19:44 +0900

    Update User : Set default value for using toObject function

--- app/src/main/java/com/gdsc_cau/vridge/data/models/User.kt
+++ app/src/main/java/com/gdsc_cau/vridge/data/models/User.kt
@@ -1,9 +1,9 @@
 package com.gdsc_cau.vridge.data.models
 
 data class User(
-    val cntVoice: Int,
-    val email: String,
-    val gender: Gender,
-    val name: String,
-    val uid: String
+    val cntVoice: Int = -1,
+    val email: String = "",
+    val gender: Gender = Gender.HIDDEN,
+    val name: String = "",
+    val uid: String = ""
 )
