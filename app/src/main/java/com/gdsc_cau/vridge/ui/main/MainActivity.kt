32839e2a92ea6dd9271b11d7861e2aa9031e2a3f
Hoon <ldh019@naver.com>
2024-02-21 02:26:51 +0900

    Update MainActivity : Add permission

--- app/src/main/java/com/gdsc_cau/vridge/ui/main/MainActivity.kt
+++ app/src/main/java/com/gdsc_cau/vridge/ui/main/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.gdsc_cau.vridge.ui.main
 
+import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import com.gdsc_cau.vridge.ui.theme.VridgeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,24 +14,37 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private val viewModel: MainViewModel by viewModels()
 
+    private val permissions = arrayOf(
+        Manifest.permission.RECORD_AUDIO
+    ).apply {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
+            plus(Manifest.permission.READ_MEDIA_AUDIO)
+        }
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { result: Map<String, Boolean> ->
+        Log.d("Permission", result.toString())
+        val deniedList: List<String> = result.filter {
+            !it.value
+        }.map {
+            it.key
+        }
+
+        if (deniedList.isNotEmpty()) {
+            finish()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestPermissionLauncher.launch(permissions)
+
         setContent {
             VridgeTheme {
-                // A surface container using the 'background' color from the theme
                 MainScreen()
             }
         }
     }
-}
-
-@Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier
-) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
