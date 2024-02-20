ca08294ea0ddc2351b2f9b037ba4f54fcdff95e3
Hoon <ldh019@naver.com>
2024-02-21 03:09:03 +0900

    Update Voice : Remove unused field

--- app/src/main/java/com/gdsc_cau/vridge/data/models/Voice.kt
+++ app/src/main/java/com/gdsc_cau/vridge/data/models/Voice.kt
@@ -1,9 +1,6 @@
 package com.gdsc_cau.vridge.data.models
 
 data class Voice(
-    val data: String,
-    val gender: Gender,
     val id: String,
-    val name: String,
-    val recorder: String,
+    val name: String
 )
