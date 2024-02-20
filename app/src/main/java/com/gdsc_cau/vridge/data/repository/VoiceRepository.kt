f9055e1fafe5751f3582ce2b37a53e3bcfad6914
Hoon <ldh019@naver.com>
2024-02-21 02:25:05 +0900

    Update VoiceRepository : Edit function signature

--- app/src/main/java/com/gdsc_cau/vridge/data/repository/VoiceRepository.kt
+++ app/src/main/java/com/gdsc_cau/vridge/data/repository/VoiceRepository.kt
@@ -3,9 +3,7 @@ package com.gdsc_cau.vridge.data.repository
 interface VoiceRepository {
     fun getTrainingText(index: Int): String
 
-    suspend fun setTrainingVoice(index: Int, filePath: String): Boolean
-
-    suspend fun makeVoice(uid: String, path: String)
+    suspend fun makeVoice(path: String): Boolean
 
     suspend fun synthesize(uid: String, voiceId: List<String>)
 
