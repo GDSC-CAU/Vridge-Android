98d4f0f0f5ef1bee0e903a6f7ae3738a394bcec6
Hoon <ldh019@naver.com>
2024-02-21 02:26:29 +0900

    Update VoiceRepositoryImpl : Implement functions

--- app/src/main/java/com/gdsc_cau/vridge/data/repository/VoiceRepositoryImpl.kt
+++ app/src/main/java/com/gdsc_cau/vridge/data/repository/VoiceRepositoryImpl.kt
@@ -1,18 +1,28 @@
 package com.gdsc_cau.vridge.data.repository
 
-import android.app.Application
+import android.content.Context
+import android.net.Uri
 import com.gdsc_cau.vridge.data.api.VridgeApi
+import com.gdsc_cau.vridge.data.database.FileStorage
+import com.google.firebase.auth.FirebaseAuth
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.JsonPrimitive
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.util.UUID
 import javax.inject.Inject
 
 class VoiceRepositoryImpl
     @Inject
     constructor(
-        private val api: VridgeApi
+        private val api: VridgeApi,
+        private val storage: FileStorage,
+        private val auth: FirebaseAuth,
+        @ApplicationContext private val context: Context
     ) : VoiceRepository {
-        private val assetManager = Application().assets
+        private val assetManager = context.assets
         private val scripts =
             assetManager.open("script.txt")
                 .let {
@@ -23,22 +33,31 @@ class VoiceRepositoryImpl
                 }
 
         override fun getTrainingText(index: Int): String {
-            return scripts[index]
+            return scripts[index - 1]
         }
 
-        override suspend fun setTrainingVoice(index: Int, filePath: String): Boolean {
-            TODO("Upload file to firebase storage")
-        }
+        override suspend fun makeVoice(path: String): Boolean {
+            val uid = auth.currentUser?.uid ?: return false
+            val vid = UUID.randomUUID().toString().replace("-", "")
+            (1..33).forEach {
+                val fileName = "$path/$it.m4a"
+                val fileReader = FileInputStream(fileName)
+                val data = ByteArray(fileReader.available())
+                fileReader.read(data)
+                storage.uploadFile(uid, vid, "$it.m4a", data)
+            }
 
-        override suspend fun makeVoice(uid: String, path: String) {
             val data = JsonObject(
                 mapOf(
                     "uid" to JsonPrimitive(uid),
-                    "path" to JsonPrimitive(path)
+                    "vid" to JsonPrimitive(vid),
+                    "path" to JsonPrimitive("$uid/$vid/train")
                 )
             )
 
-            api.uploadTrainingVoice(data)
+//            return api.uploadTrainingVoice(data)
+
+            return true
         }
 
         override suspend fun synthesize(uid: String, voiceId: List<String>) {
