394b7a906981750218c29560571c941391fba229
Hoon <ldh019@naver.com>
2024-02-21 03:10:33 +0900

    Update VoiceListViewModel : Add functions

--- app/src/main/java/com/gdsc_cau/vridge/ui/voicelist/VoiceListViewModel.kt
+++ app/src/main/java/com/gdsc_cau/vridge/ui/voicelist/VoiceListViewModel.kt
@@ -1,10 +1,33 @@
-package com.gdsc_cau.vridge.voicelist
+package com.gdsc_cau.vridge.ui.voicelist
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.gdsc_cau.vridge.data.models.Voice
+import com.gdsc_cau.vridge.data.repository.VoiceRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class VoiceListViewModel
-    @Inject
-    constructor() : ViewModel()
+class VoiceListViewModel @Inject constructor(private val repository: VoiceRepository) : ViewModel() {
+    private val _voiceList = MutableStateFlow<List<Voice>>(emptyList())
+    val voiceList: StateFlow<List<Voice>> = _voiceList
+
+    init {
+        getVoices()
+    }
+    fun getVoices() {
+        viewModelScope.launch {
+            _voiceList.emit(repository.getVoiceList())
+        }
+    }
+
+    fun synthesize(voiceList: List<String>) {
+        viewModelScope.launch {
+            repository.synthesize(voiceList)
+        }
+    }
+
+}
