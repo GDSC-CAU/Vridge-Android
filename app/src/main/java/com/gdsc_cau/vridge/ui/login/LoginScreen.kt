5899405a6e6ea919105e2b4fbf091a6b4fbcccec
yymin1022 <yymin1022@gmail.com>
2024-02-21 21:27:13 +0900

    Update LoginScreen : Improved Vridge Logo

--- app/src/main/java/com/gdsc_cau/vridge/ui/login/LoginScreen.kt
+++ app/src/main/java/com/gdsc_cau/vridge/ui/login/LoginScreen.kt
@@ -1,50 +1,67 @@
 package com.gdsc_cau.vridge.ui.login
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.gdsc_cau.vridge.R
+import com.gdsc_cau.vridge.ui.theme.IconBg
+import com.gdsc_cau.vridge.ui.theme.OnPrimary
 
 @Composable
 fun LoginScreen(onTryLogin: () -> Unit) {
-    val modifier = Modifier
-
     Column(
-        modifier = modifier.padding(all = 20.dp),
+        modifier =
+            Modifier
+                .background(IconBg)
+                .padding(all = 20.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceEvenly
     ) {
-        LoginLogo(modifier = modifier)
-        LoginButton(modifier = modifier, onTryLogin = onTryLogin)
+        LoginLogo()
+        LoginButton(onTryLogin = onTryLogin)
     }
 }
 
 @Composable
-fun LoginLogo(modifier: Modifier = Modifier) {
-    Image(
-        modifier = modifier.size(width = 200.dp, height = 200.dp),
-        painter = painterResource(id = android.R.mipmap.sym_def_app_icon),
-        contentDescription = "Login Logo"
-    )
+fun LoginLogo() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            modifier = Modifier.size(width = 200.dp, height = 200.dp),
+            painter = painterResource(id = R.drawable.ic_vridge),
+            contentDescription = "Login Logo"
+        )
+        Text(
+            text = "VRIDGE",
+            color = OnPrimary,
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
 }
 
 @Composable
 fun LoginButton(
-    modifier: Modifier = Modifier,
     onTryLogin: () -> Unit
 ) {
     Image(
         modifier =
-            modifier
+            Modifier
                 .fillMaxWidth(fraction = 0.5f)
                 .clickable { onTryLogin() },
         painter = painterResource(id = R.drawable.btn_signin_google),
