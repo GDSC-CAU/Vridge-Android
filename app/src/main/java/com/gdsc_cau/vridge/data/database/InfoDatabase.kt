b6399eae2a5618f92d38fb8dca82500fe5195f89
hoon <ldh019@naver.com>
2024-02-21 22:58:59 +0900

    Update InfoDatabase : Add functions

--- app/src/main/java/com/gdsc_cau/vridge/data/database/InfoDatabase.kt
+++ app/src/main/java/com/gdsc_cau/vridge/data/database/InfoDatabase.kt
@@ -4,6 +4,7 @@ import com.gdsc_cau.vridge.data.models.InvalidUidException
 import com.gdsc_cau.vridge.data.models.Tts
 import com.gdsc_cau.vridge.data.models.User
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
@@ -20,7 +21,7 @@ constructor(
             ?: throw InvalidUidException()
     }
 
-    suspend fun getTalks(uid: String, vid: String): List<Tts> {
+    fun getTalks(uid: String, vid: String): List<Tts> {
         val talks = mutableListOf<Tts>()
 
         database.collection("user").document(uid).collection(vid)
@@ -37,5 +38,42 @@ constructor(
                 }
             }
         return talks
+    }
+
+    fun getVoiceList(uid: String): Map<String, String> {
+        var voiceList: Map<String, String> = mapOf()
+
+        database.collection("user").document(uid)
+            .get().addOnSuccessListener { document ->
+                if (document != null) {
+                    val data = document.data
+                    if (data != null) {
+                        voiceList = data.get("voice") as Map<String, String>
+                    }
+                }
+            }
+        return voiceList
+    }
+
+    fun saveTts(uid: String, vid: String, tts: Tts) {
+        database.collection("user").document(uid).collection(vid).document(tts.id).set(
+            mapOf(
+                "text" to tts.text,
+                "timestamp" to tts.timestamp
+            )
+        )
+    }
+
+    suspend fun saveVoice(uid: String, vid: String) {
+        val result = database.collection("user").document(uid).get().await()
+        val count = result.data?.get("cntVoice") as Long
+
+        val data = hashMapOf(vid to "Voice ${count + 1}")
+        database.collection("user").document(uid).set(
+            mapOf(
+                "cntVoice" to count + 1,
+                "voice" to data
+            ), SetOptions.merge()
+        )
     }
 }
