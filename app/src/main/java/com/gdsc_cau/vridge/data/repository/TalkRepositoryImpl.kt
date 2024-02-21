25023496207fde96b4b1a8010828a781587d30f6
hoon <ldh019@naver.com>
2024-02-21 15:02:45 +0900

    Update TalkRepositoryImpl : Add functions

--- app/src/main/java/com/gdsc_cau/vridge/data/repository/TalkRepositoryImpl.kt
+++ app/src/main/java/com/gdsc_cau/vridge/data/repository/TalkRepositoryImpl.kt
@@ -2,26 +2,46 @@ package com.gdsc_cau.vridge.data.repository
 
 import com.gdsc_cau.vridge.data.api.VridgeApi
 import com.gdsc_cau.vridge.data.database.FileStorage
+import com.gdsc_cau.vridge.data.database.InfoDatabase
+import com.gdsc_cau.vridge.data.models.Tts
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.JsonPrimitive
 import javax.inject.Inject
 
 class TalkRepositoryImpl
-    @Inject
-    constructor(
-        private val api: VridgeApi,
-        private val storage: FileStorage
-    ) : TalkRepository {
-    override suspend fun getTts(text: String, uid: String, voiceId: String): String {
-            val data = JsonObject(
-                mapOf(
-                    "text" to JsonPrimitive(text),
-                    "uid" to JsonPrimitive(uid),
-                    "voiceId" to JsonPrimitive(voiceId)
-                )
+@Inject
+constructor(
+    private val api: VridgeApi,
+    private val auth: FirebaseAuth,
+    private val storage: FileStorage,
+    private val database: InfoDatabase
+) : TalkRepository {
+    override suspend fun createTts(text: String, uid: String, voiceId: String): String {
+        val data = JsonObject(
+            mapOf(
+                "text" to JsonPrimitive(text),
+                "uid" to JsonPrimitive(uid),
+                "voiceId" to JsonPrimitive(voiceId)
             )
+        )
 
-            val result = api.createTts(data)
-            TODO("File Download")
-        }
+        val result = api.createTts(data)
+        TODO("File Download")
     }
+
+    override suspend fun getTtsUrl(vid: String, ttsId: String): String {
+        val uid = auth.currentUser?.uid ?: return ""
+        return storage.getDownloadUrl("$uid/$vid/$ttsId.wav")
+    }
+
+    override suspend fun getTalks(vid: String): List<Tts> {
+        val uid = auth.currentUser?.uid ?: return emptyList()
+        return database.getTalks(uid, vid)
+    }
+
+    override suspend fun getTtsState(vid: String, ttsId: String): Boolean {
+        val uid = auth.currentUser?.uid ?: return false
+        return storage.existTts(uid, vid, ttsId)
+    }
+}
