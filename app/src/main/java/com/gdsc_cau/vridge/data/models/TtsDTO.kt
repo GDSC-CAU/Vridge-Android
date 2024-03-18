95d0790ed3f172f977ee95f0a0d6e90bb2aa76a3
hoon <ldh019@naver.com>
2024-03-18 10:55:59 +0900

    Added TtsDTO

---
+++ app/src/main/java/com/gdsc_cau/vridge/data/models/TtsDTO.kt
@@ -0,0 +1,12 @@
+package com.gdsc_cau.vridge.data.models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+data class TtsDTO(
+    @SerialName("text") val text: String,
+    @SerialName("uid") val uid: String,
+    @SerialName("vid") val vid: String,
+    @SerialName("tid") val tid: String,
+    @SerialName("pitch") val pitch: Int,
+)
